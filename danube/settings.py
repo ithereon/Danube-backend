17f129863d24d6ff9a6fd8898f6ca55f7ce12a4d
topStar419 <topstar41999@gmail.com>
2022-11-08 11:57:45 -0600

    DB config

--- danube/settings.py
+++ danube/settings.py
@@ -97,11 +97,17 @@ WSGI_APPLICATION = "danube.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
+        # "ENGINE": "django.db.backends.postgresql",
+        # "NAME": os.environ.get("DB_NAME"),
+        # "USER": os.environ.get("DB_USER"),
+        # "PASSWORD": os.environ.get("DB_PASSWORD"),
+        # "HOST": os.environ.get("DB_HOST"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'danube',
+        'USER': 'danube',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

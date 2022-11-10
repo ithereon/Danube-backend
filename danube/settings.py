d0536378e40b3ed1287212564f194caf862ab449
smith <jamesmith110111@gmail.com>
2022-11-10 06:19:11 -0800

    secret key fixed

--- danube/settings.py
+++ danube/settings.py
@@ -16,7 +16,7 @@ from django.contrib import messages
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ["SECRET_KEY"]
-SECRET_KEY = "abcdefgh"
+SECRET_KEY = "asdfghjkl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
